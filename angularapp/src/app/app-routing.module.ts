bc31e12838ac1015ca74ef51a3a264a1cf67f984
Pradeesh <pradeeshprakasan17@gmail.com>
2022-03-03 20:33:09 +0530

    routing,modules import

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   {path:"",redirectTo:'/login',pathMatch:'full'},
   {path:"login",component:LoginComponent},
   {path:"signup",component:SignupComponent},
+  {path:"home",component:HomepageComponent},
+  {path:"cart",component:CartComponent},
+  {path:"orders",component:OrdersComponent},
   {path:"**",component:NotFoundComponent}
 ];
 
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CartComponent } from './cart/cart.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { OrdersComponent } from './orders/orders.component';
 import { UserNavbarComponent } from './user-navbar/user-navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
@@ -19,10 +22,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SignupComponent,
     LoginComponent,
     NotFoundComponent,
-    CartComponent,
     HomepageComponent,
-    OrdersComponent,
-    UserNavbarComponent
+    UserNavbarComponent,
+    CartComponent,
+    OrdersComponent 
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
