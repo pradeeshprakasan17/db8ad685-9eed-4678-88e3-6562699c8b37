cd436ac6441948de80c1155e516c2ecd4d80f715
Pradeesh <pradeeshprakasan17@gmail.com>
2022-03-13 19:45:42 +0530

    produt[]

--- angularapp/src/app/homepage/homepage.component.ts
+++ angularapp/src/app/homepage/homepage.component.ts
@@ -10,22 +10,7 @@ import { CartService } from '../cart-service/cart.service';
 })
 export class HomepageComponent implements OnInit {
   
-  productItems : Product[] = [
-    { id:1,
-      product_name: "Key_Chain",
-      product_price: 1000,
-      product_img: "./../assets/images/login-main.png",
-      product_desc: "This gift items from the amaze pack online site",
-      product_quantity: 10
-    },
-    { id:2,
-      product_name: "Chain",
-      product_price: 1500,
-      product_img: "./../assets/images/login-main.png",
-      product_desc: "This chain items from the amaze pack online site",
-      product_quantity: 5
-    },
-  ];
+  productItems : Product[] = [];
 
   gridColumns = 3;
 
